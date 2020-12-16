3dcddcb4a63e96a9167d16bacacbe810ba75f037
Joshua Hooper <joshuahooper3@gmail.com>
2020-12-16 12:40:37 -0600

    First Commit of Consecutive Number Checker

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            System.Console.WriteLine("Enter a few numbers separated by hyphens: ");
+            var rawText = Console.ReadLine();
+
+            string[] numberStrings = rawText.Split('-');
+            int[] numberInts = Array.ConvertAll(numberStrings, int.Parse);
+
+            for (var i = 0; i < numberInts.Length; i++)
+            {
+                if (i == numberInts.Length - 1)
+                {
+                    Console.WriteLine("Consecutive");
+                }
+                else if (numberInts[i+1] >= numberInts[i])
+                {
+                    continue;
+                }
+                else
+                {
+                    Console.WriteLine("Not Consecutive");
+                    break;
+                }
+            }
+        }
+    }
+}
